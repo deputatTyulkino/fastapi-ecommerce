9958b71f166e73c59951795df986ff69256cab3f
Tigran Petrosyan <tigr-080807@mail.ru>
2026-03-15 02:56:08 +0500

    Создание таблиц Category и Product

---
+++ app/models/products.py
@@ -0,0 +1,24 @@
+from decimal import Decimal
+from typing import TYPE_CHECKING
+
+from app.database import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, Numeric, Boolean, ForeignKey
+
+if TYPE_CHECKING:
+    from .categories import Category
+
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str | None] = mapped_column(String(500))
+    price: Mapped[Decimal] = mapped_column(Numeric(10, 2), nullable=False)
+    image_url: Mapped[str | None] = mapped_column(String(200))
+    stock: Mapped[int] = mapped_column(Integer, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'), nullable=False)
+
+    category: Mapped["Category"] = relationship("Category", back_populates='products')
