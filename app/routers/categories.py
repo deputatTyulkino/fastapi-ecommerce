41a5e171c2e6ff641a2503903dd567519849c720
Tigran Petrosyan <tigr-080807@mail.ru>
2026-03-15 21:57:13 +0500

    Написание api для категорий

--- app/routers/categories.py
+++ app/routers/categories.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi import status
+from sqlalchemy.orm import Session
+from sqlalchemy import select, update
+from app.db_depends import get_db
+from app.schemas.categories import CategorySchema, CategoryCreate
+from app.models.categories import Category
 
 router = APIRouter(
     prefix='/categories',
@@ -6,33 +12,86 @@ router = APIRouter(
 )
 
 
-@router.get('/')
-async def get_all_categories() -> dict:
+@router.get('/', response_model=list[CategorySchema])
+async def get_all_categories(db: Session = Depends(get_db)):
     """
     Возвращает список всех категорий товаров.
     """
-    return {"message": "List of all categories"}
+    categories = db.scalars(
+        select(Category).where(Category.is_active == True)
+    ).all()
+    return categories
 
 
-@router.post('/')
-async def create_category() -> dict:
+@router.post('/', response_model=CategorySchema, status_code=status.HTTP_201_CREATED)
+async def create_category(category: CategoryCreate, db: Session = Depends(get_db)):
     """
     Создаёт новую категорию.
     """
-    return {"message": "Create new category"}
+    if category.parent_id is not None:
+        parent = db.scalars(
+            select(Category)
+            .where(Category.id == category.parent_id, Category.is_active == True)
+        ).first()
+        if parent is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail='Parent category not found'
+            )
+    db_category = Category(**category.model_dump())
+    db.add(db_category)
+    db.commit()
+    db.refresh(db_category)
+    return db_category
 
 
-@router.put('/{category_id}')
-async def update_category(category_id: int) -> dict:
+@router.put('/{category_id}', response_model=CategorySchema)
+async def update_category(
+        category_id: int, category: CategoryCreate, db: Session = Depends(get_db)
+) -> dict:
     """
     Обновляет категорию по её ID.
     """
-    return {"message": f"Category with ID: {category_id} updated"}
+    db_category = db.scalars(
+        select(Category).where(Category.id == category_id, Category.is_active == True)
+    ).first()
+    if category is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='Category not found'
+        )
+    if category.parent_id is not None:
+        parent = db.scalars(
+            select(Category).where(
+                Category.id == category.parent_id, Category.is_active == True
+            )
+        ).first()
+        if parent is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail='Parent category not found'
+            )
+    db.execute(
+        update(Category)
+        .where(Category.id == category_id)
+        .values(**category.model_dump())
+    )
+    db.commit()
+    db.refresh(db_category)
+    return db_category
 
 
-@router.delete('/{category_id}')
-async def delete_category(category_id: int) -> dict:
+@router.delete('/{category_id}', status_code=status.HTTP_200_OK)
+async def delete_category(category_id: int, db: Session = Depends(get_db)) -> dict:
     """
     Удаляет категорию по её ID.
     """
-    return {"message": f"Category with ID: {category_id} deleted"}
+    category = db.scalars(
+        select(Category).where(Category.id == category_id, Category.is_active == True)
+    ).first()
+    if category is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='Category not found'
+        )
+    db.execute(
+        update(Category).where(Category.id == category_id).values(is_active=False)
+    )
+    db.commit()
+    return {"status": "success", "message": "Category marked as inactive"}
--- app/schemas/categories.py
+++ app/schemas/categories.py
@@ -7,18 +7,18 @@ class CategoryCreate(BaseModel):
     Используется в POST и PUT запросах.
     """
     name: str = Field(
-        min_length=3, max_length=50, description='Название категории'
+        ..., min_length=3, max_length=50, description='Название категории'
     )
     parent_id: int | None = Field(None, description='ID родительской категории')
 
 
-class Category(BaseModel):
+class CategorySchema(BaseModel):
     """
     Модель для ответа с данными категории.
     Используется в GET-запросах.
     """
-    id: int = Field(description='Уникальный идентификатор категории')
-    name: str = Field(description='Название категории')
+    id: int = Field(..., description='Уникальный идентификатор категории')
+    name: str = Field(..., description='Название категории')
     parent_id: int | None = Field(None, description='ID родительской категории')
-    is_active: bool = Field(description='Активность категории')
+    is_active: bool = Field(..., description='Активность категории')
     model_config = ConfigDict(from_attributes=True)
