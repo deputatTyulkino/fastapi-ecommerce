6ab899a299044bdae93569fb44fd7c68d5219dc0
Tigran Petrosyan <tigr-080807@mail.ru>
2026-03-17 22:09:15 +0500

    Переход на асинхронное api для категорий

--- app/routers/categories.py
+++ app/routers/categories.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi import status
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, update
 from app.db_depends import get_db
 from app.schemas.categories import CategorySchema, CategoryCreate
@@ -13,85 +13,93 @@ router = APIRouter(
 
 
 @router.get('/', response_model=list[CategorySchema])
-async def get_all_categories(db: Session = Depends(get_db)):
+async def get_all_categories(db: AsyncSession = Depends(get_db)):
     """
     Возвращает список всех категорий товаров.
     """
-    categories = db.scalars(
+    stmt = await db.scalars(
         select(Category).where(Category.is_active == True)
-    ).all()
+    )
+    categories = stmt.all()
     return categories
 
 
 @router.post('/', response_model=CategorySchema, status_code=status.HTTP_201_CREATED)
-async def create_category(category: CategoryCreate, db: Session = Depends(get_db)):
+async def create_category(category: CategoryCreate, db: AsyncSession = Depends(get_db)):
     """
     Создаёт новую категорию.
     """
     if category.parent_id is not None:
-        parent = db.scalars(
+        stmt = await db.scalars(
             select(Category)
             .where(Category.id == category.parent_id, Category.is_active == True)
-        ).first()
+        )
+        parent = stmt.first()
         if parent is None:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail='Parent category not found'
             )
     db_category = Category(**category.model_dump())
     db.add(db_category)
-    db.commit()
-    db.refresh(db_category)
+    await db.commit()
     return db_category
 
 
 @router.put('/{category_id}', response_model=CategorySchema)
 async def update_category(
-        category_id: int, category: CategoryCreate, db: Session = Depends(get_db)
-) -> dict:
+        category_id: int, category: CategoryCreate, db: AsyncSession = Depends(get_db)
+):
     """
     Обновляет категорию по её ID.
     """
-    db_category = db.scalars(
+    stmt = await db.scalars(
         select(Category).where(Category.id == category_id, Category.is_active == True)
-    ).first()
-    if category is None:
+    )
+    db_category = stmt.first()
+    if db_category is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail='Category not found'
         )
     if category.parent_id is not None:
-        parent = db.scalars(
+        parent_stmt = await db.scalars(
             select(Category).where(
                 Category.id == category.parent_id, Category.is_active == True
             )
-        ).first()
-        if parent is None:
+        )
+        parent = parent_stmt.first()
+        if not parent:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail='Parent category not found'
             )
-    db.execute(
+        if parent.id == category_id:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail='Category cannot be its own parent'
+            )
+    updated_data = category.model_dump(exclude_unset=True)
+    await db.execute(
         update(Category)
         .where(Category.id == category_id)
-        .values(**category.model_dump())
+        .values(**updated_data)
     )
-    db.commit()
-    db.refresh(db_category)
+    await db.commit()
     return db_category
 
 
 @router.delete('/{category_id}', status_code=status.HTTP_200_OK)
-async def delete_category(category_id: int, db: Session = Depends(get_db)) -> dict:
+async def delete_category(category_id: int, db: AsyncSession = Depends(get_db)) -> dict:
     """
     Удаляет категорию по её ID.
     """
-    category = db.scalars(
+    stmt = await db.scalars(
         select(Category).where(Category.id == category_id, Category.is_active == True)
-    ).first()
+    )
+    category = stmt.first()
     if category is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail='Category not found'
         )
-    db.execute(
+    await db.execute(
         update(Category).where(Category.id == category_id).values(is_active=False)
     )
-    db.commit()
+    await db.commit()
     return {"status": "success", "message": "Category marked as inactive"}
