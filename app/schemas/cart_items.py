87727f487ba73560621b6f532ad72c97943ff524
Tigran Petrosyan <tigr-080807@mail.ru>
2026-04-11 04:25:23 +0500

    Создание схем для корзины

---
+++ app/schemas/cart_items.py
@@ -0,0 +1,35 @@
+from app.database import Base
+from pydantic import Field, ConfigDict
+
+from app.schemas.products import Product
+
+
+class CartItemBase(Base):
+    product_id: int = Field(description='ID товара')
+    quantity: int = Field(description='Количество товара')
+
+
+class CartItemCreate(CartItemBase):
+    """Модель для добавления нового товара в корзину."""
+
+
+class CartItemUpdate(CartItemBase):
+    """Модель для обновления количества товара в корзине."""
+    quantity: int = Field(..., ge=1, description='Новое количество товара')
+
+
+class CartItem(CartItemBase):
+    """Товар в корзине с данными продукта."""
+    id: int = Field(..., description='ID позиции корзины')
+    quantity: int = Field(..., ge=1, description='Количество товара')
+    product: Product = Field(..., description='Информация о товаре')
+    model_config = ConfigDict(from_attributes=True)
+
+
+class Cart(CartItemBase):
+    """Полная информация о корзине пользователя."""
+    user_id: int = Field(..., description='ID пользователя')
+    items: list[CartItem] = Field(default_factory=list, description='Содержимое корзины')
+    total_quantity: int = Field(..., ge=0, description='Общее количество товаров')
+    total_price: int = Field(..., ge=0, description='Общая стоимость товаров')
+    model_config = ConfigDict(from_attributes=True)
