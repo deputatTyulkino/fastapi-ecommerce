089231b40e9de0e07797294e3c22bc9d48440040
Tigran Petrosyan <tigr-080807@mail.ru>
2026-04-11 04:12:21 +0500

    Создание схемы, описывающий ответ получения отфильтрованного списка товаров

--- app/schemas/products.py
+++ app/schemas/products.py
@@ -30,3 +30,14 @@ class Product(BaseModel):
     category_id: int = Field(..., description='ID категории')
     is_active: bool = Field(..., description='Активность товара')
     model_config = ConfigDict(from_attributes=True)
+
+
+class ProductList(BaseModel):
+    """
+    Список пагинации для товаров.
+    """
+    items: list[Product] = Field(description='Товары для текущей страницы')
+    total: int = Field(ge=0, description='Общее количество товаров')
+    page: int = Field(ge=1, description='Номер текущей страницы')
+    page_size: int = Field(ge=1, description='Количество элементов на странице')
+    model_config = ConfigDict(from_attributes=True)
