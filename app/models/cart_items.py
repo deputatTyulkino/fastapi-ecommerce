d7869d21b8ae42a520c517e0a188772ce8fa0c52
Tigran Petrosyan <tigr-080807@mail.ru>
2026-04-11 05:26:52 +0500

    Добавление модели корзины

--- app/models/cart_items.py
+++ app/models/cart_items.py
@@ -1,6 +1,5 @@
 from typing import TYPE_CHECKING
-
-from pydantic import BaseModel
+from app.database import Base
 from sqlalchemy import UniqueConstraint, ForeignKey, DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
@@ -10,11 +9,11 @@ if TYPE_CHECKING:
     from .products import Product
 
 
-class CartItem(BaseModel):
+class CartItem(Base):
     __tablename__ = 'cart_items'
 
     __table_args__ = (
-        UniqueConstraint('user_id', 'product_id', name='uq_cart_items_user_product')
+        UniqueConstraint('user_id', 'product_id', name='uq_cart_items_user_product'),
     )
 
     id: Mapped[int] = mapped_column(primary_key=True)
